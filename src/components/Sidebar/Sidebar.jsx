cbdbd8cd21e581422c077878d2730181f811ae54
UnknownXstark <nathanlouiskalio@gmail.com>
2025-07-09 22:05:47 +0100

    updates

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from "react";
 import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
 
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
-        <div className='sidebar-header'>
-            <h1>BetterTasks</h1>
-            <MdKeyboardDoubleArrowLeft />
-        </div>
-        <div className='sidebar-main-menu'>
-            p
-        </div>
-        <div className='sidebar-lists'></div>
-        <div className='sidebar-extra-options'></div>
-        <div className='sidebar-profile'></div>
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <h1>BetterTasks</h1>
+        <MdKeyboardDoubleArrowLeft />
+      </div>
+      <div className="sidebar-main-menu">
+        <p>MAIN MENU</p>
+        <button>To-Do</button>
+        <button>Share My Impact</button>
+        <button>Analytics</button>
+        <button>Leaderboard</button>
+      </div>
+      <div className="sidebar-lists"></div>
+      <div className="sidebar-extra-options"></div>
+      <div className="sidebar-profile"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
