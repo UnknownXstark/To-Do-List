071f1c9bae2c6f9079bb62577374b82ea6ee6049
UnknownXstark <nathanlouiskalio@gmail.com>
2025-07-09 22:06:26 +0100

    added icons to my sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
+import { BiBarChartAlt2 } from "react-icons/bi";
+import { DiGoogleAnalytics } from "react-icons/di";
+import { CiBullhorn } from "react-icons/ci";
+import { GoChecklist } from "react-icons/go";
 
 const Sidebar = () => {
   return (
@@ -10,10 +14,22 @@ const Sidebar = () => {
       </div>
       <div className="sidebar-main-menu">
         <p>MAIN MENU</p>
-        <button>To-Do</button>
-        <button>Share My Impact</button>
-        <button>Analytics</button>
-        <button>Leaderboard</button>
+        <button>
+          <GoChecklist />
+          To-Do
+        </button>
+        <button>
+          <CiBullhorn />
+          Share My Impact
+        </button>
+        <button>
+          <DiGoogleAnalytics />
+          Analytics
+        </button>
+        <button>
+          <BiBarChartAlt2 />
+          Leaderboard
+        </button>
       </div>
       <div className="sidebar-lists"></div>
       <div className="sidebar-extra-options"></div>
