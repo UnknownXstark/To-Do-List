357483fa671989828766185f0236898699e76077
UnknownXstark <nathanlouiskalio@gmail.com>
2025-07-09 22:03:49 +0100

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>Good Morning!</h1>
+        <p>What do you plan to do toady?</p>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className='sidebar-header'>
+            
+        </div>
+        <div className='sidebar-main-menu'></div>
+        <div className='sidebar-lists'></div>
+        <div className='sidebar-extra-options'></div>
+        <div className='sidebar-profile'></div>
+    </div>
+  )
+}
+
+export default Sidebar
