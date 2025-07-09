1c07956b8560734ec4abbcb350375afa76aedce1
UnknownXstark <nathanlouiskalio@gmail.com>
2025-07-09 22:05:08 +0100

    started my layout for the sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
         <div className='sidebar-header'>
-            
+            <h1>BetterTasks</h1>
+            <MdKeyboardDoubleArrowLeft />
         </div>
-        <div className='sidebar-main-menu'></div>
+        <div className='sidebar-main-menu'>
+            p
+        </div>
         <div className='sidebar-lists'></div>
         <div className='sidebar-extra-options'></div>
         <div className='sidebar-profile'></div>
