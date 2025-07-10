50a13ba916acf5e7a387f7279042740434b0b506
UnknownXstark <nathanlouiskalio@gmail.com>
2025-07-10 12:16:58 +0100

    updates to layout

--- src/components/Task Card/TaskkCard.jsx
+++ src/components/Task Card/TaskkCard.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import './TaskCard.css'
+import React from "react";
+import "./TaskCard.css";
 import { CgMathPlus } from "react-icons/cg";
 
 const TaskkCard = () => {
+  const [tasks, setTasks] = useState([]);
   return (
-    <div className='taskcard'>
-        <div className='taskcard-head'>
-            <h1>Todays Task.</h1>
-        </div>
-        <div className='taskcard-tasks'></div>
-        <div className='taskcard-footer'>
-            <button>Finish</button>
-            <button><CgMathPlus /> Add task</button>
-        </div>
-    </div>
-  )
-}
+    <div className="taskcard">
+      <div className="taskcard-head">
+        <h1>Todays Task.</h1>
+      </div>
+      <div className="taskcard-tasks">
+        <form>
+          <input type="text" placeholder="What are your plans?" />
 
-export default TaskkCard
+          <button>Finish</button>
+          <button>
+            <CgMathPlus /> Add task
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default TaskkCard;
