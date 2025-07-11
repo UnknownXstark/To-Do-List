6f9faccee7c74443af6d330e0aa98b6ca12854ee
UnknownXstark <nathanlouiskalio@gmail.com>
2025-07-11 15:24:05 +0100

    changes to the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
+import TaskkCard from "./components/Task Card/TaskkCard";
 
 function App() {
   return (
     <>
       <Sidebar />
+      <TaskkCard />
     </>
   );
 }
--- src/components/Task Card/TaskkCard.jsx
+++ src/components/Task Card/TaskkCard.jsx
@@ -29,24 +29,6 @@ const TaskkCard = () => {
         <h1>Todays Task.</h1>
       </div>
       <div className="taskcard-tasks">
-        {tasks.map((task) => (
-          <div className="task-item" key={task.id}>
-            <input
-              type="checkbox"
-              checked={task.completed}
-              onChange={() => handleToggleComplete(task.id)}
-            />
-            <span
-              className={`priority-tag ${task.completed ? "completed" : ""}`}
-            >
-              {task.title}
-            </span>
-            <span className={`priority-tag ${task.priority.toLowerCase()}`}>
-              {task.priority}
-            </span>
-            <button className="finish-btn">Finish</button>
-          </div>
-        ))}
         <form onSubmit={handleAddTask}>
           <input
             type="text"
@@ -55,11 +37,31 @@ const TaskkCard = () => {
             onChange={(e) => setNewTask(e.target.value)}
           />
 
-          <button type="submit">Finish</button>
-          <button type="button">
+          {/* <button type="submit">Finish</button> */}
+          <button type="submit">
             <CgMathPlus /> Add task
           </button>
         </form>
+        <ul className="taskcard-list">
+          {tasks.map((task) => (
+            <div className="task-item" key={task.id}>
+              <input
+                type="checkbox"
+                checked={task.completed}
+                onChange={() => handleToggleComplete(task.id)}
+              />
+              <span
+                className={`priority-tag ${task.completed ? "completed" : ""}`}
+              >
+                {task.title}
+              </span>
+              <span className={`priority-tag ${task.priority.toLowerCase()}`}>
+                {task.priority}
+              </span>
+              {/* <button className="finish-btn">Finish</button> */}
+            </div>
+          ))}
+        </ul>
       </div>
     </div>
   );
